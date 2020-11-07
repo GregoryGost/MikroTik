f0e6c9a02d8bd59d812dc7e0cb0055e831f5871a
GregoryGost <huzver@gmail.com>
2020-11-07 04:15:37 +0300

    Update dd if of

--- CHR/make-chr.sh
+++ CHR/make-chr.sh
@@ -13,10 +13,18 @@
 ROS="6.46.8" && \
 PASSWORD="CHANGEME" && \
 apt-get update && \
-apt install -y unzip && \
+apt install -y unzip qemu-utils pv && \
+mount -t tmpfs tmpfs /tmp/ && \
+cd /tmp
 wget https://download.mikrotik.com/routeros/$ROS/chr-$ROS.img.zip -O chr.img.zip  && \
 gunzip -c chr.img.zip > chr.img  && \
-mount -o loop,offset=33554944 chr.img /mnt && \
+qemu-img convert chr.img -O qcow2 chr.qcow2  && \
+modprobe nbd  && \
+qemu-nbd -c /dev/nbd0 chr.qcow2  && \
+sleep 5 && \
+partprobe /dev/nbd0 && \
+sleep 5 && \
+mount /dev/nbd0p1 /mnt && \
 ADDRESS=`ip addr show eth0 | grep global | cut -d' ' -f 6 | head -n 1` && \
 GATEWAY=`ip route list | grep default | cut -d' ' -f 3` && \
 PASSWORD="CHANGEME" && \
@@ -34,15 +42,10 @@ echo "/ip address add address=$ADDRESS interface=[/interface ethernet find where
 /system package update install
  " > /mnt/rw/autorun.scr && \
 umount /mnt && \
+dd if=/dev/nbd0 of=/dev/vda bs=4M oflag=sync && \
 sleep 5 && \
-echo u > /proc/sysrq-trigger && \
+killall qemu-nbd && \
 sleep 5 && \
-echo "Writing raw image, this will take time" && \
-dd if=chr.img bs=1024 of=/dev/vda && \
-echo "Don't forget your password: $PASSWORD" && \
-sleep 5 || true && \
-echo "sync disk" && \
-echo s > /proc/sysrq-trigger && \
-sleep 5 || true && \
-echo "Ok, reboot" && \
+echo "Ok, hard reboot" && \
+echo 1 > /proc/sys/kernel/sysrq
 echo b > /proc/sysrq-trigger
