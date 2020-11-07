84ea045e068a3dcb07df52296ac5944b1f8525ee
GregoryGost <huzver@gmail.com>
2020-11-07 03:41:34 +0300

    Redesign
    redesigned for simplicity
    https://github.com/hreskiv/chr-eve-ng/blob/master/chr-vultr.sh

--- CHR/make-chr.sh
+++ CHR/make-chr.sh
@@ -1,56 +1,47 @@
 #!/bin/bash
 #
-# Digital Ocean Ubuntu 16.04 Droplet
+# Digital Ocean Ubuntu, Debian
+#
 # Running:
-# 
+# wget https://raw.githubusercontent.com/GregoryGost/MikroTik/master/CHR/make-chr.sh
 # chmod +x make-chr.sh
 # ./make-chr.sh
 #
-wget https://download.mikrotik.com/routeros/6.46.8/chr-6.46.8.img.zip -O chr.img.zip  && \
-gunzip -c chr.img.zip > chr.img  && \
+# ROS change your version
+# PASSWORD must be changes !!!
+#
+ROS="6.46.8"
 apt-get update && \
-apt install -y qemu-utils pv && \
-qemu-img convert chr.img -O qcow2 chr.qcow2 && \
-qemu-img resize chr.qcow2 1073741824 && \
-modprobe nbd && \
-qemu-nbd -c /dev/nbd0 chr.qcow2 && \
-echo "Give some time for qemu-nbd to be ready" && \
-sleep 2 && \
-partprobe /dev/nbd0 && \
-sleep 5 && \
-mount /dev/nbd0p2 /mnt && \
+apt install -y unzip && \
+wget https://download.mikrotik.com/routeros/$ROS/chr-$ROS.img.zip -O chr.img.zip  && \
+gunzip -c chr.img.zip > chr.img  && \
+mount -o loop,offset=33554944 chr.img /mnt && \
 ADDRESS=`ip addr show eth0 | grep global | cut -d' ' -f 6 | head -n 1` && \
 GATEWAY=`ip route list | grep default | cut -d' ' -f 3` && \
 PASSWORD="CHANGEME" && \
 echo "/ip address add address=$ADDRESS interface=[/interface ethernet find where name=ether1]
 /ip route add gateway=$GATEWAY
 /ip service disable telnet
+/ip service disable ftp
+/ip service disable www
+/ip service disable api
+/ip service disable api-ssl
 /user set 0 name=root password=$PASSWORD
 /ip dns set servers=1.1.1.1,1.0.0.1
+/system package update set channel=long-term
+/system package update check-for-updates
 /system package update install
  " > /mnt/rw/autorun.scr && \
 umount /mnt && \
-echo "Magic constant is 65537 (second partition address). You can check it with fdisk before appliyng this" && \
-echo "This scary sequence removes seconds partition on nbd0 and creates new, but bigger one" && \
-echo -e 'd\n2\nn\np\n2\n65537\n\nw\n' | fdisk /dev/nbd0 && \
-e2fsck -f -y /dev/nbd0p2 || true && \
-resize2fs /dev/nbd0p2 && \
-sleep 1 && \
-echo "Compressing to gzip, this can take several minutes" && \
-mount -t tmpfs tmpfs /mnt && \
-pv /dev/nbd0 | gzip > /mnt/chr-extended.gz && \
-sleep 1 && \
-killall qemu-nbd && \
-sleep 1 && \
+sleep 5 && \
 echo u > /proc/sysrq-trigger && \
-echo "Warming up sleep" && \
-sleep 1 && \
+sleep 5 && \
 echo "Writing raw image, this will take time" && \
-zcat /mnt/chr-extended.gz | pv > /dev/vda && \
+dd if=chr.img bs=1024 of=/dev/vda && \
 echo "Don't forget your password: $PASSWORD" && \
-echo "Sleep 5 seconds (if lucky)" && \
 sleep 5 || true && \
 echo "sync disk" && \
 echo s > /proc/sysrq-trigger && \
+sleep 5 || true && \
 echo "Ok, reboot" && \
 echo b > /proc/sysrq-trigger
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MikroTik
- MikroTik scripts, settings, etc.
+MikroTik scripts, settings, etc.
+
+## CHR (Cloud Hosted Router)
+Script for install CHR on Digital Ocean Ubuntu 16.04 Droplet  
+[Download last CHR image](https://mikrotik.com/download)
+
+Fork script: [https://gist.github.com/stroebs/54fc09734a3911e91eeeb43434f117df](https://gist.github.com/stroebs/54fc09734a3911e91eeeb43434f117df)
